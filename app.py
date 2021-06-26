4cc83043e57c7b4f02ed82a0129c2533cebb2057
shun-uscpa <49315514+shun-uscpa@users.noreply.github.com>
2021-06-26 18:10:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ def generate_table(dataframe, max_rows=10):
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 app.layout = html.Div(children=[
     html.H4(children='US Agriculture Exports (2011)'),
